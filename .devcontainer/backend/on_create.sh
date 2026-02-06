e777ebab46e877ef943408aed03b2283e6152674
Dan Stace <dstace@gmail.com>
2026-02-06 02:06:04 +0000

    source venvrc in each shell

--- .devcontainer/backend/on_create.sh
+++ .devcontainer/backend/on_create.sh
@@ -3,6 +3,4 @@ source .devcontainer/bootstrap.sh
 pip install -r backend/requirements.txt
 pip install -r dev/requirements.txt
 
-sudo echo "eval \"$(scaf)\"" >> ~/.bashrc
-sudo echo "eval \"$(scaf .)\"" >> ~/.bashrc
-sudo echo "alias | grep scaf" >> ~/.bashrc
+sudo echo "source .venvrc" >> ~/.bashrc
