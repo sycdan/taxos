043bdc788c41d3f6bdd5519de95529acfc1add30
Dan Stace <dstace@gmail.com>
2026-02-07 02:54:09 +0000

    explicit union type

--- backend/taxos/access/token/generate/command.py
+++ backend/taxos/access/token/generate/command.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from typing import Union
 
 from taxos.tenant.entity import Tenant, TenantRef
 
 
 @dataclass
 class GenerateAccessToken:
-  tenant: Tenant | TenantRef
+  tenant: Union[Tenant, TenantRef]
 
   def execute(self):
     from taxos.access.token.generate.handler import handle
