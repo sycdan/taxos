85f311d429b87ffe9fa53d3a6b2e75159c75b2f9
Dan Stace <dstace@domain.com>
2026-02-08 18:26:56 +0000

    simplify

--- backend/taxos/index_unallocated_receipts/handler.py
+++ backend/taxos/index_unallocated_receipts/handler.py
@@ -11,51 +11,45 @@ from taxos.tools import json
 logger = logging.getLogger(__name__)
 
 
-def handle(command: IndexUnallocatedReceipts) -> list[Receipt]:
+def _process(receipt_ref: ReceiptRef, unallocated_receipts: dict[str, set[str]]):
+  try:
+    receipt = receipt_ref.hydrate()
+  except Receipt.DoesNotExist:
+    logger.debug(f"Skipping receipt with missing or invalid state: {receipt_ref}")
+    return
+  if receipt.is_unallocated:
+    month = receipt.date.strftime("%Y-%m")
+    logger.debug(f"Indexing unallocated receipt for month {month}: {receipt.guid}")
+    unallocated_receipts.setdefault(month, set()).add(receipt.guid.hex)
+
+
+def handle(command: IndexUnallocatedReceipts) -> dict[str, set[str]]:
+  unallocated_receipts: dict[str, set[str]] = {}  # month -> guids
   tenant = require_tenant(command.tenant)
 
-  logger.info(f"Indexing unallocated receipts for tenant {tenant.guid}")
-  unallocated_receipts: list[Receipt] = []
-  index: dict[str, set[str]] = {}  # month -> guids
   receipts_dir = get_receipts_dir(tenant.guid)
-  unallocated_file = get_unallocated_file(tenant.guid)
-  if unallocated_file.exists() and unallocated_file.stat().st_size > 0:
-    logger.info(f"Loading existing unallocated receipts index for tenant {tenant.guid}")
-    state = json.load(unallocated_file)
-    index.update({k: set(v) for k, v in state.items()})
-
   if not receipts_dir.exists():
     logger.info(f"Creating receipts directory for tenant {tenant.guid}")
     os.makedirs(receipts_dir, exist_ok=True)
 
-  content_dirs = []
+  logger.info(f"Indexing unallocated receipts for tenant {tenant.guid}")
+  unallocated_file = get_unallocated_file(tenant.guid)
+  if unallocated_file.exists() and unallocated_file.stat().st_size > 0:
+    logger.info(f"Loading existing unallocated receipts index for tenant {tenant.guid}")
+    state = json.load(unallocated_file)
+    unallocated_receipts.update({k: set(v) for k, v in state.items()})
+
   if command.receipt:
-    try:
-      receipt = command.receipt.hydrate()
-      content_dirs.append(receipt.state_file.parent)
-    except Receipt.DoesNotExist:
-      pass
+    logger.info(f"Indexing single receipt {command.receipt} for tenant {tenant.guid}")
+    _process(command.receipt, unallocated_receipts)
   else:
-    content_dirs.extend(receipts_dir.iterdir())
+    for receipt_dir in receipts_dir.iterdir():
+      if receipt_dir.is_dir():
+        _process(ReceiptRef(receipt_dir.name), unallocated_receipts)
 
-  for receipt_dir in content_dirs:
-    if not receipt_dir.is_dir():
-      logger.debug(f"Skipping non-directory item: {receipt_dir}")
-      continue
-
-    if not (receipt := ReceiptRef(receipt_dir.name).hydrate()):
-      logger.debug(f"Skipping receipt with missing or invalid state: {receipt_dir.name}")
-      continue
-
-    if receipt.is_unallocated:
-      unallocated_receipts.append(receipt)
-      month = receipt.date.strftime("%Y-%m")
-      logger.debug(f"Indexing unallocated receipt for month {month}: {receipt.guid}")
-      index.setdefault(month, set()).add(receipt.guid.hex)
-
-  logger.info(f"Found {len(index)} unallocated receipts")
+  logger.info(f"Found {len(unallocated_receipts)} unallocated receipts")
 
   with unallocated_file.open("w") as f:
-    json.dump(index, f)
+    json.dump(unallocated_receipts, f)
 
   return unallocated_receipts
