88fd43a701347736637de7004dc0cf5b2f474fa9
Dan Stace <dstace@domain.com>
2026-02-08 05:40:38 +0000

    fix delete bucket

--- backend/taxos/bucket/delete/handler.py
+++ backend/taxos/bucket/delete/handler.py
@@ -1,14 +1,22 @@
+import logging
 import shutil
 
 from taxos.bucket.delete.command import DeleteBucket
-from taxos.bucket.load.query import LoadBucket
+from taxos.bucket.tools import get_state_file
+from taxos.context.tools import require_tenant
+
+logger = logging.getLogger(__name__)
 
 
 def handle(command: DeleteBucket):
+  logger.debug(f"{command=}")
+  tenant = require_tenant()
   try:
-    bucket = LoadBucket(ref=command.ref).execute()
-    if bucket.content_dir.exists():
-      shutil.rmtree(bucket.content_dir)
+    bucket_guid = command.ref.guid
+    state_file = get_state_file(bucket_guid, tenant.guid)
+    content_dir = state_file.parent
+    if content_dir.exists():
+      shutil.rmtree(content_dir)
       return True
   except RuntimeError:
     pass  # probably does not exist
