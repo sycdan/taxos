6b37a3ced97d1cdd5d19a36be2c3403fbf85ee15
Dan Stace <dstace@gmail.com>
2026-02-11 23:00:34 -0500

    frontend

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm -rf backend/data/tenants/0698940164917d1e80005b91d725b497/buckets/
 rm -rf backend/data/tenants/0698940164917d1e80005b91d725b497/receipts/
-docker compose up backend -d
+docker compose up -d
 cd frontend
 npm run test:integration #-- -t "should handle receipt allocations correctly"
