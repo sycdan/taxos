79dcabaca158b7aa45c62b67b683694a4671e831
Dan Stace <dstace@domain.com>
2026-02-14 23:55:35 +0000

    ignore scratch script

--- dev/scratch/command.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Scratch:
-  """Run the thing I am working on right now."""
--- dev/scratch/handler.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from dev.scratch.command import Scratch
-
-
-def handle(command: Scratch):
-  os.system("docker compose build")
-  os.system("docker run -it --rm taxos-backend bash")
